211584571b4a514f15b6f398c41e84afbc4e43b3
Alden <31806638+aldnwong@users.noreply.github.com>
2024-12-09 21:42:54 -0800

    Add notifications to PlayerData

--- src/main/java/ong/aldenw/managers/PlayerManager.java
+++ src/main/java/ong/aldenw/managers/PlayerManager.java
@@ -1,8 +1,13 @@
 package ong.aldenw.managers;
 
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtString;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -42,21 +47,33 @@ public class PlayerManager {
     public static class PlayerData {
         private String username;
         private final UUID uuid;
+        private final ArrayList<String> notifications;
 
         public PlayerData(String username, UUID uuid) {
             this.username = username;
             this.uuid = uuid;
+            notifications = new ArrayList<>();
         }
 
         public PlayerData(NbtCompound compound) {
             this.username = compound.getString("username");
             this.uuid = UUID.fromString(compound.getString("uuid"));
+
+            this.notifications = new ArrayList<>();
+            NbtList notificationNbt = compound.getList("notifications", NbtElement.STRING_TYPE);
+            notificationNbt.forEach(notification -> notifications.add(notification.asString()));
         }
 
         public NbtCompound writeNbt() {
             NbtCompound pdNbt = new NbtCompound();
             pdNbt.putString("username", username);
             pdNbt.putString("uuid", uuid.toString());
+
+            NbtList notificationNbt = new NbtList();
+            notifications.add("you left :(");
+            notifications.forEach(notification -> notificationNbt.add(NbtString.of(notification)));
+            pdNbt.put("notifications", notificationNbt);
+
             return pdNbt;
         }
     }
